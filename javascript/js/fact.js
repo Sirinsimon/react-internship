0e74b2785e08d9b06145f74aba66b38bee6fae93
Sirinsimon <sirinolical@gmail.com>
2026-06-15 10:37:21 +0530

    jss

---
+++ javascript/js/fact.js
@@ -0,0 +1,14 @@
+sum = 0 
+for(var i = 1 ; i <= 50 ;i++){
+    sum += i
+}
+console.log("Sum of 50 no: "+sum)
+
+fact = 1
+
+for(var i = 1; i <= 5;i++){
+    fact *= i
+}
+
+console.log("Factorial of 5 is : " +fact)
+
